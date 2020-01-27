de762b219c2971cffb6137f4baf1956195d1d186
Hubert <zajaczkowski.hubert@outlook.con>
2020-01-27 03:12:33 +0100

    Crypto added

---
+++ Sigma.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace BUS_DAA_SIGMA
+{
+    class Sigma
+    {
+        private readonly Random _random;
+        private readonly int    _bigIntBufferLength;
+
+        private BigInteger _s;
+        private BigInteger _g;
+        private BigInteger _x;
+
+        public Sigma()
+        {
+            _random             = new Random();
+            _bigIntBufferLength = 256;
+        }
+
+        public void BeginKeyExchange()
+        {
+            _s = GetRandomPositiveBigInteger();
+            _g = new BigInteger();
+            _x = GetRandomPositiveBigInteger();
+        }
+
+        private BigInteger GetRandomPositiveBigInteger()
+        {
+            byte[] buffer = new byte[_bigIntBufferLength];
+            _random.NextBytes(buffer);
+            buffer[_bigIntBufferLength - 1] &= 0x7F; // Force sign bit to positive.  See https://docs.microsoft.com/en-us/dotnet/api/system.numerics.biginteger.-ctor.
+            return new BigInteger(buffer);
+        }
+    }
+}
